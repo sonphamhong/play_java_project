52139fbc01128877aa651733af5ba425e3b87ac6
DucNV <duc.nv@anlab.vn>
2015-05-26 17:58:55 +0700

    Change language per request
    Change-Id: Id97b46b303c7b1fa5e2d98b92d6407c5f7027c1c

--- src/app/Global.java
+++ src/app/Global.java
@@ -18,6 +18,12 @@ import java.lang.reflect.Method;
 
 import play.i18n.Lang;
 import play.i18n.Messages;
+import play.GlobalSettings;
+import play.libs.F.Promise;
+import play.mvc.Action;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.SimpleResult;
 
 
 
@@ -34,6 +40,27 @@ public class Global extends GlobalSettings {
             Ebean.save(all.get("companies"));
           }
         }
+    }
+    
+    private class ActionWrapper extends Action.Simple {
+        public ActionWrapper(Action<?> action) {
+            this.delegate = action;
+        }
+
+        @Override
+        public Promise<SimpleResult> call(Http.Context ctx) throws java.lang.Throwable {
+        	String locale = ctx.request().getQueryString("locale");
+        	ctx.changeLang(locale);
+        	Promise<SimpleResult> result = this.delegate.call(ctx);
+            Http.Response response = ctx.response();
+            //response.setHeader("Access-Control-Allow-Origin", "*");
+            return result;
+        }
+    }
+
+    @Override
+    public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
+        return new ActionWrapper(super.onRequest(request, actionMethod));
     }
     
 }
--- src/app/controllers/Application.java
+++ src/app/controllers/Application.java
@@ -79,22 +79,4 @@ public class Application extends Controller {
     //Lang.apply(locale);
     return redirect(routes.Application.lists());
   }
-
-
-//  public Action onRequest(final Http.Request request, final Method actionMethod) {
-//  
-//      if (request.host().equals("de.yourdomain.tld")
-//              && (request.cookie("PLAY_LANG") == null || !request.cookie("PLAY_LANG").value().equals("de"))) {
-//          return new Action.Simple() {
-//              public Result call(Http.Context ctx) throws Throwable {
-//                  ctx.changeLang("de");
-//                  return redirect(request.path());
-//              }
-//          };
-//      } else {
-//          return super.onRequest(request, actionMethod);
-//      }
-//  }
-  
-  
 }
